dd183a351dc48a9c6ca859f4befb48c76050463a
ivan tactuk mercado <ivantm24@gmail.com>
2017-05-27 14:19:45 -0400

    initial commit

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import water_level_sensor as wls
+import pump_controller as pump
+import notifications as ntf
+
+
+
+while True:
+    if wls.fishes_tank.overflowing():
+        pass
+    elif wls.fishes_tank.lacking_water():
+        pass
+
+    if wls.plants_tank.overflowing():
+        if pump.is_on():
+            ntf.send("ALERT!!! - Aquaponics", "Plants ")
+        pump.turn_off()
+    elif wls.plants_tank.lacking_water():
+        pass
---
+++ src/pump_controller.py
@@ -0,0 +1,18 @@
+import aquaponics_gpio as agpio
+import RPi.GPIO as GPIO
+
+_is_on = True
+
+
+def is_on():
+    return _is_on
+
+
+def turn_on():
+    _is_on = True
+    GPIO.output(agpio.PUMP_SIGNAL_PIN, True)
+
+
+def turn_off():
+    _is_on = False
+    GPIO.output(agpio.PUMP_SIGNAL_PIN, False)
