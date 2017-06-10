527c27d0fd285cf30c3c1690226a9c894f3675fd
natanco <natancorummage@gmail.com>
2017-06-10 16:40:21 -0400

    Return temp only in F and only once

--- src/temp_sensor.py
+++ src/temp_sensor.py
@@ -32,9 +32,7 @@ def read_temp():
         temp_string = lines[1][equals_pos + 2:]
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c, temp_f
+        return temp_f
 
 
-while True:
-    print(read_temp())
-    time.sleep(1)
+print(read_temp())
