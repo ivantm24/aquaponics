ae3ffe0bbad1e30174a3890eb1617bf3e5111837
natanco <natancorummage@gmail.com>
2017-06-10 16:42:12 -0400

    fishes_tank.overflowing will return true if sensor goes low

--- src/water_level_sensor.py
+++ src/water_level_sensor.py
@@ -1,10 +1,16 @@
+# Note: When plunger is down the sensor reads high
+# When the plunger is up the sensor reads low
+
 import aquaponics_gpio as agpio
 
 class fishes_tank:
 
     @staticmethod
     def overflowing():
-        return False
+        if GPIO.input(FISHES_TANK_PIN) == 0:                #NOTE:: Need to verify functionality
+            return True
+        else:
+            return False
 
     @staticmethod
     def lacking_water():
