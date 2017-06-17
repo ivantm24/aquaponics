4ac56280d6ce059ce483879d157a7a7a9eb9463f
ivan tactuk mercado <ivantm24@gmail.com>
2017-06-17 15:42:45 -0400

    updated main so it doesn't resends a notification until it has been cleared

--- src/main.py
+++ src/main.py
@@ -8,21 +8,31 @@ initialize_GPIO()
 fish_tank_notification_sent = False
 plant_tank_notification_sent = False
 
-while True:
-    if wls.fishes_tank.overflowing():
-        if not fish_tank_notification_sent:
-            print("Fish tank overflowing")
-            fish_tank_notification_sent = True
-    else:
-        fish_tank_notification_sent = False
-        if fish_tank_notification_sent:
-            print("Fish tank is not longer overflowing")
+flags={}
 
-    if wls.plants_left.overflowing():
-        print("Left plant tank overflowing")
+def verify_send_alert(alert_method, activated_message, message_deactivate):
+    if alert_method not in flags:
+        flags[alert_method] = False
+    if alert_method():
+        if not flags[alert_method]:
+            print activated_message
+            flags[alert_method] = True
+    else:
+        if flags[alert_method]:
+            print message_deactivate
+        flags[alert_method] = False
+
+
+while True:
+    verify_send_alert(wls.fishes_tank.overflowing,"Fish tank overflowing","Fish tank is not longer overflowing")
+
+    verify_send_alert(wls.plants_left.overflowing, "Left plant tank overflowing", "Left plant tank is not longer overflowing")
+
+    # if wls.plants_left.overflowing():
+    #     print("Left plant tank overflowing")
 #        if pump.is_on():
  #           ntf.send("ALERT!!! - Aquaponics", "Plants ")           NOTE:: uncomment when adding notifications
  #       pump.turn_off()
+    verify_send_alert(wls.plants_right.overflowing, "Right plant tank overflowing",
+                      "Right plant tank is not longer overflowing")
 
-    if wls.plants_right.overflowing():
-        print("Right plant tank overflowing")
