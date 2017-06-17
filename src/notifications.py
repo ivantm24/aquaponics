69ff3303de35d7e82265a7280c70fefc9cd45f69
ivan tactuk mercado <ivantm24@gmail.com>
2017-06-16 20:48:51 -0400

    added config file to load the configuration for the notifications. allowed multiple recipients

--- src/notifications.py
+++ src/notifications.py
@@ -1,15 +1,19 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import json
 
-fromaddr = "aquaponics.rit@gmail.com"
-password = input("Password: ")
-toaddr = input("Receiver's email: ")
+config = json.loads(open("config.json").read())
+config = config["email_config"]
+fromaddr = config["email"]
+password = config["password"]
+mailing_list = config["mailing_list"]
+recipients =mailing_list
 
 def send(Subject, Message):
     msg = MIMEMultipart()
     msg['From'] = fromaddr
-    msg['To'] = toaddr
+    msg['To'] = ", ".join(mailing_list)
     msg['Subject'] = Subject
 
     body = Message
@@ -19,5 +23,5 @@ def send(Subject, Message):
     server.starttls()
     server.login(fromaddr, password)
     text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
+    server.sendmail(fromaddr, recipients, text)
     server.quit()
